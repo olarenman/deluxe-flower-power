ac12216e8179381ed05af3cb003b15b8958f36bc
olarenman <ola.renman@gmail.com>
2016-05-17 11:58:13 +0200

    Create chart.js

---
+++ www/chart.js
@@ -0,0 +1,36 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    
+    var jsonData = $.ajax({
+	url: "getData.php",
+	dataType: "json",
+	async: false}).responseText;
+
+    jsonData = JSON.parse(jsonData);
+
+    var data = new google.visualization.DataTable();
+
+    data.addColumn('datetime', 'Tid');
+    data.addColumn('number', 'Fuktighet');
+    
+    for(var i = 0; i < jsonData.length; i++) {
+	var t = jsonData[i].time.split(/[- :]/);
+	var date = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+	data.addRow([date, jsonData[i].data]);
+    }
+
+    var options = {
+        title: 'Dlxflwpwr',
+	curveType: 'function',
+	legend: { position: 'bottom' },
+	hAxis: { format: 'dd/MM/yyyy HH:mm',
+		 slantedText : 'true',
+		 textStyle: {fontSize: 12}
+	       }	
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+    chart.draw(data, options);
+}
