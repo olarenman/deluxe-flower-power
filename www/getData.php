a647a96288ca1a457e2c882a11ba466a7e090a12
olarenman <ola.renman@gmail.com>
2016-05-17 14:42:55 +0200

    Add files via upload

---
+++ www/getData.php
@@ -0,0 +1,21 @@
+<?php 
+
+      //open connection to mysql db
+      $connection = mysqli_connect("localhost","dlxflwpwr","dlxflwpwr","data")
+      or die("Error " . mysqli_error($connection));
+
+      $sql = "SELECT * FROM (SELECT time, data FROM dlxflwpwr ORDER BY time DESC LIMIT 20) AS x ORDER BY time";
+
+      $result = mysqli_query($connection, $sql)
+      or die("Error in Selecting " . mysqli_error($connection));
+
+      $temparray = array();
+
+      while($row = mysqli_fetch_assoc($result))
+      {
+	$temparray[] = $row;
+	}
+
+      echo json_encode($temparray);
+
+?>
