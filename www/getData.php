45b466bcfb601acf44c4e7507432b0228d1b7ee0
olarenman <ola.renman@gmail.com>
2016-05-17 15:03:21 +0200

    Update getData.php

--- www/getData.php
+++ www/getData.php
@@ -4,7 +4,7 @@
       $connection = mysqli_connect("localhost","dlxflwpwr","dlxflwpwr","data")
       or die("Error " . mysqli_error($connection));
 
-      $sql = "SELECT * FROM (SELECT time, data FROM dlxflwpwr ORDER BY time DESC LIMIT 20) AS x ORDER BY time";
+      $sql = "SELECT * FROM (SELECT time, data FROM data ORDER BY time DESC LIMIT 20) AS x ORDER BY time";
 
       $result = mysqli_query($connection, $sql)
       or die("Error in Selecting " . mysqli_error($connection));
