ec307bee3c67ba48ce0682c69bdb369a73362874
olarenman <ola.renman@gmail.com>
2016-05-17 15:24:10 +0200

    Update chart.js

--- www/chart.js
+++ www/chart.js
@@ -18,7 +18,7 @@ function drawChart() {
     for(var i = 0; i < jsonData.length; i++) {
 	var t = jsonData[i].time.split(/[- :]/);
 	var date = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
-	data.addRow([date, jsonData[i].data]);
+	data.addRow([date, parseFloat(jsonData[i].data)]);
     }
 
     var options = {
